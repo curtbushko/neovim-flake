ed84f5f3da0803df5727f09f28bbbe89a21ba8c7
Curt Bushko <cbushko@gmail.com>
2024-10-22 20:33:57 -0400

    update lsp and conform for go

--- config/plugins/go.nix
+++ config/plugins/go.nix
@@ -6,14 +6,63 @@
   plugins = {
     lsp.servers.gopls = {
       enable = true;
+      package = null;
+      settings = {
+        codelenses = {
+          gc_details = false;
+          generate = true;
+          regenerate_cgo = true;
+          run_govulncheck = true;
+          test = true;
+          tidy = true;
+          upgrade_dependency = true;
+          vendor = true;
+        };
+        hints = {
+          assignVariableTypes = true;
+          compositeLiteralFields = true;
+          compositeLiteralTypes = true;
+          constantValues = true;
+          functionTypeParameters = true;
+          parameterNames = true;
+          rangeVariableTypes = true;
+        };
+        analyses = {
+          fieldalignment = true;
+          nilness = true;
+          unusedparams = true;
+          unusedwrite = true;
+          useany = true;
+        };
+        usePlaceholders = true;
+        completeUnimported = true;
+        staticcheck = true;
+        directoryFilters = [
+          "-.git"
+          "-.vscode"
+          "-.idea"
+          "-.vscode-test"
+          "-node_modules"
+        ];
+        semanticTokens = true;
+      };
     };
+
     conform-nvim.settings = {
-      formatters_by_ft.go = {
-        __unkeyed-goimports = "goimports";
-        __unkeyed-gofmt = "gofmt";
+      format_on_save = {
+        lspFallback = true;
+        timeoutMs = 500;
+      };
+      formatters_by_ft = {
+        go = [
+          "goimports"
+          "gofmt"
+          "gofumpt"
+        ];
       };
       formatters = {
         gofmt.command = lib.getExe' pkgs.go "gofmt";
+        gofumpt.command = lib.getExe' pkgs.go "gofumt";
         goimports.command = lib.getExe' pkgs.gotools "goimports";
       };
     };
